3a04f81a4c4ef629eb0ae52b6316348d4a1ea744
Varun Kumar <varunkumarsunkaraboina@gmail.com>
2025-07-03 14:29:19 +0530

    first commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export const axiosInstance=axios.create({
-    baseURL:'http://localhost:5001',
+    baseURL:'https://talkie-backend-snowy.vercel.app/',
     withCredentials:true
 })
